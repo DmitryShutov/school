16e0fd01a80423776e75ab85753c5a75fee5278f
dmitry.shutov <dmitry.shutov@ringcentral.com>
2023-07-26 13:02:53 +0300

    fix naming, removed unused imports

--- PowerSet.java
+++ PowerSet.java
@@ -1,8 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -22,7 +20,7 @@ public class PowerSet {
         slots = new String[MAX_SIZE];
         size = 0;
         Arrays.stream(values)
-              .filter(v -> !Objects.isNull(v))
+              .filter(v -> v != null)
               .forEach(v -> put((String) v));
     }
 
@@ -38,7 +36,7 @@ public class PowerSet {
 
     public OptionalInt seekSlot(String value)
     {   
-        return Arrays.stream(getIndexes(value))
+        return Arrays.stream(getIndices(value))
                 .filter(i -> value == null || slots[i] == null || slots[i].equals(value))
                 .limit(1L)
                 .findFirst();
@@ -58,10 +56,10 @@ public class PowerSet {
     }
 
     public boolean get(String value) {
-        return Arrays.stream(getIndexes(value)).anyMatch(i -> value.equals(slots[i]));
+        return Arrays.stream(getIndices(value)).anyMatch(i -> value.equals(slots[i]));
     }
 
-    private int[] getIndexes(String value) {
+    private int[] getIndices(String value) {
         return IntStream.concat(IntStream.range(hashFun(value), slots.length), IntStream.range(0, hashFun(value))).toArray();
     }
 
