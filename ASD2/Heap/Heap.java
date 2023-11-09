0c55f2ca3c9a20e6bbc0216cac9c573cb47cb328
Dmitrii Shutov <7dmitry77@gmail.com>
2023-11-09 18:50:13 +0400

    ASD2 heap

--- ASD2/Heap/Heap.java
+++ ASD2/Heap/Heap.java
@@ -2,81 +2,66 @@ package Heap;
 
 class Heap {
     public int[] HeapArray; // хранит неотрицательные числа-ключи
+    public int size; // размер фактический
 
     public Heap() {
         HeapArray = null;
+        size = 0;
     }
 
     public void MakeHeap(int[] a, int depth) {
         int heapSize = (int) Math.pow(2, depth + 1) - 1;
         HeapArray = new int[heapSize];
-        for (int i = 0; i < heapSize; i++) HeapArray[i] = -1;
         for (int j : a) {
             Add(j);
         }
     }
 
     public int GetMax() {
-        if (HeapArray[0] == -1) {
-            return -1;
-        }
-        int max = HeapArray[0];
-        for (int index = 0; index < HeapArray.length; index++) {
-            if (HeapArray[index] == -1) {
-                break;
-            }
-            int leftChildIndex = 2 * index + 1;
-            int rightChildIndex = 2 * index + 2;
-            if (HeapArray[leftChildIndex] == -1 && HeapArray[rightChildIndex] == -1) {
-                break;
-            }
-            if (HeapArray[leftChildIndex] > HeapArray[rightChildIndex]) {
-                HeapArray[index] = HeapArray[leftChildIndex];
-                HeapArray[leftChildIndex] = -1;
-                index = leftChildIndex - 1;
-            } else {
-                HeapArray[index] = HeapArray[rightChildIndex];
-                HeapArray[rightChildIndex] = -1;
-                index = rightChildIndex - 1;
-            }
-        }
-        return max;
+        if (size == 0) return -1;
+        int result = HeapArray[0];
+        HeapArray[0] = HeapArray[size - 1];
+        HeapArray[size - 1] = 0;
+        size--;
+        siftDown(0);
+        return result;
     }
 
     public boolean Add(int key) {
-        if (HeapArray[0] == -1) {
-            HeapArray[0] = key;
-            return true;
-        }
-        for (int index = 0; index < HeapArray.length; index++) {
-            int leftChildIndex = 2 * index + 1;
-            int rightChildIndex = 2 * index + 2;
-            if (leftChildIndex >= HeapArray.length || rightChildIndex >= HeapArray.length) {
-                return false;
-            }
-            if (HeapArray[leftChildIndex] == -1) {
-                HeapArray[leftChildIndex] = key;
-                break;
-            }
-            if (HeapArray[rightChildIndex] == -1) {
-                HeapArray[rightChildIndex] = key;
-                break;
-            }
-            if (HeapArray[leftChildIndex] > HeapArray[rightChildIndex]) {
-                index = leftChildIndex - 1;
-            } else {
-                index = rightChildIndex - 1;
-            }
-        }
+        if (size == HeapArray.length) return false;
+        HeapArray[size] = key;
+        siftUp(size);
+        size++;
         return true;
     }
 
-    public int FindEmptyIndex() {
-        for (int i = HeapArray.length - 1; i > 0; i--) {
-            if (HeapArray[i] < 0) {
-                return i;
+    private void siftDown(int index) {
+        int maxIndex = index;
+        while (index < size / 2) {
+            int leftChild = 2 * index + 1;
+            int rightChild = 2 * index + 2;
+            if (leftChild < size && HeapArray[leftChild] > HeapArray[maxIndex]) {
+                maxIndex = leftChild;
             }
+            if (rightChild < size && HeapArray[rightChild] > HeapArray[maxIndex]) {
+                maxIndex = rightChild;
+            }
+            if (maxIndex == index) {
+                break;
+            }
+            int temp = HeapArray[index];
+            HeapArray[index] = HeapArray[maxIndex];
+            HeapArray[maxIndex] = temp;
+            index = maxIndex;
         }
-        return 0;
+    }
+
+    private void siftUp(int index) {
+        while (index > 0 && HeapArray[(index - 1) / 2] < HeapArray[index]) {
+            int temp = HeapArray[(index - 1) / 2];
+            HeapArray[(index - 1) / 2] = HeapArray[index];
+            HeapArray[index] = temp;
+            index = (index - 1) / 2;
+        }
     }
 }
--- ASD2/Heap/HeapTest.java
+++ ASD2/Heap/HeapTest.java
@@ -3,6 +3,7 @@ package Heap;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HeapTest {
     @Test
@@ -16,15 +17,38 @@ public class HeapTest {
     public void makeHeapTwoEl() {
         Heap heap = new Heap();
         heap.MakeHeap(new int[]{1, 2}, 1);
-        assertArrayEquals(new int[]{2, 1, -1}, heap.HeapArray);
+        assertArrayEquals(new int[]{2, 1, 0}, heap.HeapArray);
     }
-/*
-    public void MakeHeap() {
+
+    @Test
+    public void MakeLongHeap() {
         Heap heap = new Heap();
         heap.MakeHeap(new int[]{1, 2, 3, 4, 5, 6, 7}, 2);
         assertArrayEquals(new int[]{7, 4, 6, 1, 3, 2, 5}, heap.HeapArray);
     }
 
+    @Test
+    public void GetOne() {
+        Heap heap = new Heap();
+        heap.MakeHeap(new int[]{1}, 0);
+        assertEquals(1, heap.GetMax());
+        assertEquals(0, heap.HeapArray[0]);
+    }
 
- */
+    @Test
+    public void GetTwoElementsHeap() {
+        Heap heap = new Heap();
+        heap.MakeHeap(new int[]{1, 2}, 1);
+        assertEquals(2, heap.GetMax());
+        assertEquals(1, heap.HeapArray[0]);
+        assertEquals(0, heap.HeapArray[1]);
+    }
+
+    @Test
+    public void twiceGetLongHeap() {
+        Heap heap = new Heap();
+        heap.MakeHeap(new int[]{1, 2, 3, 4, 5, 6, 7}, 2);
+        assertEquals(7, heap.GetMax());
+        assertEquals(6, heap.GetMax());
+    }
 }
